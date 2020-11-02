5cf95bd3356480bc00646230dad0d7536e0fd89b
wasabi315 <wasabi.nvim.st@gmail.com>
2020-11-02 12:21:41 +0900

    Ok: with debug

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,7 +6,23 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-// <<<remove this comment and fill your code here if needed>>>
+void writeNumber(unsigned char n) {
+    int n1 = n / 100;
+    int n2 = (n % 100) / 10;
+    int n3 = n % 10;
+
+    if (n1 != 0) {
+        char c1[1] = {n1 + '0'};
+        write(1, c1, 1);
+    }
+    if (n2 != 0) {
+        char c2[1] = {n2 + '0'};
+        write(1, c2, 1);
+    }
+    char c3[1] = {n3 + '0'};
+    write(1, c3, 1);
+    return;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -20,7 +36,42 @@ int main(int argc, char *argv[]) {
     // tick value before starting rounds
     int start_tick = uptime();
 
-    // <<<remove this comment and fill your code here>>>
+    int p[2];
+    pipe(p);
+    int pid = fork();
+    if (pid == 0) { /* child */
+        unsigned char cnt;
+        for (int i = 0; i < n; i++) {
+            read(p[0], &cnt, 1);
+
+            write(1, "Child read: ", 12);
+            writeNumber(cnt);
+            write(1, "\n", 1);
+
+            cnt++;
+            write(p[1], &cnt, 1);
+        }
+
+        close(p[0]);
+        close(p[1]);
+        exit(0);
+    } else { /* parent */
+        unsigned char cnt = 0;
+        for (int i = 0; i < n; i++) {
+            write(p[1], &cnt, 1);
+            read(p[0], &cnt, 1);
+
+            write(1, "Parent read: ", 13);
+            writeNumber(cnt);
+            write(1, "\n", 1);
+
+            cnt++;
+        }
+
+        close(p[0]);
+        close(p[1]);
+        wait(0);
+    }
 
     // print # of ticks in nrounds
     printf("# of ticks in %d rounds: %d\n", n, uptime() - start_tick);
